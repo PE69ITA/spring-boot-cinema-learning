359e70aafda0cc401fe0c9d53b472a38531bf9a1
PE69ItA <kuk72942@email.com>
2026-07-27 17:08:22 +0600

    Implement JWT service and authentication filter

--- src/main/java/com/example/cinema/security/JwtService.java
+++ src/main/java/com/example/cinema/security/JwtService.java
@@ -1,5 +1,6 @@
 package com.example.cinema.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 
 @Service
@@ -24,6 +26,34 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis()+expiration))
                 .signWith(getSigningKey())
                 .compact();
+    }
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claimsResolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
     }
 
     private SecretKey getSigningKey(){
